8bbff84c8d52d76a4f3a38c9ec7039181950ab1b
Michael Arrasmith <michael.keith.arrasmith@gmail.com>
2023-09-17 16:45:16 -0400

    added game rules and added styling

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 
 // WHEN I click the start button
 // THEN a timer starts and I am presented with a question
-//event listener on a button..//function(event) with a timer
+//event listener on a button..//function(event) with a timer  
 
 // WHEN I answer a question
 // THEN I am presented with another question
@@ -41,27 +41,53 @@ var currentQuestion = 0;
 var questions = [
   {
     number: 1,
-    text: 'this is a question?',
-    answers: ['answer one', 'answer two', 'answer three', 'answer 4'],
-    correctAnswer: 'answer one'
+    text: 'Question 1 | What does HTML stand for?',
+    answers: ['HyperText Markup Language', 'HydroTech Medium Lowpoint', 'Hexagon Track Mile Log', 'HeliTract Missed Loop'],
+    correctAnswer: 'HyperText Markup Language'
   },
   {
     number: 2,
-    text: 'this is a question?',
-    answers: ['answer one', 'answer two', 'answer three', 'answer 4'],
-    correctAnswer: 'answer one'
+    text: 'Who invented HTML?',
+    answers: ['Sir Isaac Newton', 'Sir John Williams', 'Sir Tim Berners-Lee', 'Sir Patrick Stewart'],
+    correctAnswer: 'Sir Tim Berners-Lee'
   },
   {
     number: 3,
-    text: 'this is a question?',
-    answers: ['answer one', 'answer two', 'answer three', 'answer 4'],
-    correctAnswer: 'answer one'
-  }, {
+    text: 'When was HTML created?',
+    answers: ['1985', '2001', '1992', '1990'],
+    correctAnswer: '1990'
+  }, 
+  {
     number: 4,
     text: 'this is a question?',
     answers: ['answer one', 'answer two', 'answer three', 'answer 4'],
     correctAnswer: 'answer one'
   },
+  {
+    number: 4,
+    text: 'this is a question?',
+    answers: ['answer one', 'answer two', 'answer three', 'answer 4'],
+    correctAnswer: 'answer one'
+  },
+  {
+    number: 4,
+    text: 'this is a question?',
+    answers: ['answer one', 'answer two', 'answer three', 'answer 4'],
+    correctAnswer: 'answer one'
+  },
+  {
+    number: 4,
+    text: 'this is a question?',
+    answers: ['answer one', 'answer two', 'answer three', 'answer 4'],
+    correctAnswer: 'answer one'
+  },
+  {
+    number: 4,
+    text: 'this is a question?',
+    answers: ['answer one', 'answer two', 'answer three', 'answer 4'],
+    correctAnswer: 'answer one'
+  },
+
 ]
 function populateQuestion() {
   // Grab one question from the questions array using currentQuestion index
