60fc429c3ea1d823494b0706b19c672fddb4ed00
Michael Arrasmith <michael.keith.arrasmith@gmail.com>
2023-09-18 17:40:56 -0400

    added more multiple choice questions and cleaned up layout

--- script.js
+++ script.js
@@ -1,25 +1,3 @@
-// GIVEN I am taking a code quiz
-
-
-
-//event listener on a button..//function(event) with a timer  
-
-
-//array..multiple choice
-
-
-
-// WHEN all questions are answered or the timer reaches 0
-// THEN the game is over
-
-// WHEN the game is over
-// THEN I can save my initials and my score
-
-// 1. User clicks a button -> put question on page and start timer
-// 2. 
-
-
-// ----------------------------
 var pressPlay = document.getElementById("btn");
 var questionField = document.getElementById("questionField");
 var answerField0 = document.querySelector("#answerField0");
@@ -29,6 +7,51 @@ var answerField3 = document.querySelector("#answerField3");
 var countdown = document.querySelector(".countdown-timer");
 var timeLeft = 90;
 var userScore = 0;
+var currentQuestion = 0;
+var questions = [
+  {
+    number: 1,
+    text: 'Question 1 | What does HTML stand for?',
+    answers: ['a) HyperText Markup Language', 'b) HydroTech Medium Lowpoint', 'c) Hexagon Track Mile Log', 'd) HeliTract Missed Loop'],
+    correctAnswer: 'a) HyperText Markup Language'
+  },
+  {
+    number: 2,
+    text: 'Question 2 | Who invented HTML?',
+    answers: ['a) Sir Isaac Newton', 'b) Sir John Williams', 'c) Sir Tim Berners-Lee', 'd) Sir Patrick Stewart'],
+    correctAnswer: 'c) Sir Tim Berners-Lee'
+  },
+  {
+    number: 3,
+    text: 'Question 3 | When was HTML created?',
+    answers: ['a) 1985', 'b) 2001', 'c) 1992', 'd) 1990'],
+    correctAnswer: 'd) 1990'
+  },
+  {
+    number: 4,
+    text: 'Question 4 | Which of the following is NOT a primitive value type?',
+    answers: ['a) Number', 'b) String', 'c) Window', 'd) Boolean'],
+    correctAnswer: 'c) Window'
+  },
+  {
+    number: 5,
+    text: 'Question 5 | What is the array index of this question?',
+    answers: ['a) 3', 'b) 6', 'c) 0', 'd) 4'],
+    correctAnswer: 'd) 4'
+  },
+  {
+    number: 6,
+    text: 'Question 6 | What does DOM stand for?',
+    answers: ['a) Digital Objective Mandate', 'b) Design Orientated Master', 'c) Detriment Optional Method', 'd) Document Object Model'],
+    correctAnswer: 'd) Document Object Model'
+  },
+  {
+    number: 7,
+    text: 'Question 7 | Which of the following allows a user to input information into the window?',
+    answers: ['a) window.log', 'b) window.prompt', 'c) window.alert', 'd) window.input'],
+    correctAnswer: 'b) window.prompt'
+  },
+]
 
 // WHEN I click the start button
 pressPlay.addEventListener("click", startGame);
@@ -43,6 +66,19 @@ function startGame() {
   startTimer();
 }
 
+// Grab one question from the questions array using currentQuestion index
+//button with clickevent calls populatequestion and send populatequestion number
+function populateQuestion() {
+
+  questionField.innerHTML = questions[currentQuestion].text;
+
+  for (var i = 0; i < questions[currentQuestion].answers.length; i++) {
+    var answerField = document.getElementById("answerField" + i);
+
+    answerField.innerHTML = questions[currentQuestion].answers[i];
+  }
+}
+
 function compareAnswer(event) {
   console.log(event.target.innerText);
   console.log(questions[currentQuestion].correctAnswer);
@@ -51,6 +87,7 @@ function compareAnswer(event) {
     window.alert("That's Correct!");
     userScore=userScore+5;
     window.alert("Your score is now " + userScore)
+    endGame()
   }
 // WHEN I answer a question incorrectly
 // THEN time is subtracted from the clock
@@ -71,80 +108,16 @@ function startTimer() {
 
     if (timeLeft === 0) {
       clearInterval(timeRemaining);
-      window.alert("you lose");
-      return startGame();
+      endGame();
     }
   }, 1000);
 }
+var storedScore = 
 
-
-// Grab one question from the questions array using currentQuestion index
-//button with clickevent calls populatequestion and send populatequestion number
-function populateQuestion() {
-
-  questionField.innerHTML = questions[currentQuestion].text;
-
-  for (var i = 0; i < questions[currentQuestion].answers.length; i++) {
-    var answerField = document.getElementById("answerField" + i);
-
-    answerField.innerHTML = questions[currentQuestion].answers[i];
-  }
+// WHEN all questions are answered or the timer reaches 0
+// THEN the game is over
+// WHEN the game is over
+// THEN I can save my initials and my score
+function endGame() {
+  window.stop();
 }
-
-var currentQuestion = 0;
-
-var questions = [
-  {
-    number: 1,
-    text: 'Question 1 | What does HTML stand for?',
-    answers: ['a) HyperText Markup Language', 'b) HydroTech Medium Lowpoint', 'c) Hexagon Track Mile Log', 'd) HeliTract Missed Loop'],
-    correctAnswer: 'a) HyperText Markup Language'
-  },
-  {
-    number: 2,
-    text: 'Who invented HTML?',
-    answers: ['a) Sir Isaac Newton', 'b) Sir John Williams', 'c) Sir Tim Berners-Lee', 'd) Sir Patrick Stewart'],
-    correctAnswer: 'c) Sir Tim Berners-Lee'
-  },
-  {
-    number: 3,
-    text: 'When was HTML created?',
-    answers: ['a) 1985', 'b) 2001', 'c) 1992', 'd) 1990'],
-    correctAnswer: 'd) 1990'
-  },
-  {
-    number: 4,
-    text: 'this is a question?',
-    answers: ['answer one', 'answer two', 'answer three', 'answer 4'],
-    correctAnswer: 'answer one'
-  },
-  {
-    number: 5,
-    text: 'this is a question?',
-    answers: ['answer one', 'answer two', 'answer three', 'answer 4'],
-    correctAnswer: 'answer one'
-  },
-  {
-    number: 6,
-    text: 'this is a question?',
-    answers: ['answer one', 'answer two', 'answer three', 'answer 4'],
-    correctAnswer: 'answer one'
-  },
-  {
-    number: 7,
-    text: 'this is a question?',
-    answers: ['answer one', 'answer two', 'answer three', 'answer 4'],
-    correctAnswer: 'answer one'
-  },
-  {
-    number: 8,
-    text: 'this is a question?',
-    answers: ['answer one', 'answer two', 'answer three', 'answer 4'],
-    correctAnswer: 'answer one'
-  },
-
-]
-
-
-// function countdown()
-
