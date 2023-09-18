c7b9561376d07a5ceeb1869c93eae19a0cd62db9
Michael Arrasmith <michael.keith.arrasmith@gmail.com>
2023-09-17 22:25:27 -0400

    added js for loop and potential answers

--- script.js
+++ script.js
@@ -7,11 +7,6 @@
 // WHEN I answer a question
 // THEN I am presented with another question
 //array..multiple choice
-function startGame() {
-  populateQuestion();
-  // start the timer
-  // populate a question
-}
 
 // WHEN I answer a question incorrectly
 // THEN time is subtracted from the clock
@@ -25,15 +20,54 @@ function startGame() {
 // 1. User clicks a button -> put question on page and start timer
 // 2. 
 
-// ----------------------------
-var questionField = document.getElementById("questionField")
 
+// ----------------------------
+var pressPlay = document.getElementById("btn");
+var questionField = document.getElementById("questionField");
+var answerField0 = document.querySelector("#answerField0");
+var answerField1 = document.querySelector("#answerField1");
+var answerField2 = document.querySelector("#answerField2");
+var answerField3 = document.querySelector("#answerField3");
+
+
+pressPlay.addEventListener("click", startGame);
+answerField0.addEventListener("click", compareAnswer);
+answerField1.addEventListener("click", compareAnswer);
+answerField2.addEventListener("click", compareAnswer);
+answerField3.addEventListener("click", compareAnswer);
+
+
+//   // start the timer
+//   // populate a question
 function startGame() {
   populateQuestion();
-  // start the timer
-  // populate a question
 }
 
+function compareAnswer(event) {
+  console.log(event.target.innerText)
+  console.log(questions[currentQuestion].correctAnswer)
+
+  // if (questions[currentQuestion].correctAnswer)
+  // If the answer is the same as the correct answer then the game progresses to the next question
+  //The current question index increases by 1
+  //then the next question  
+}
+
+
+// Grab one question from the questions array using currentQuestion index
+//button with clickevent calls populatequestion and send populatequestion number
+function populateQuestion() {
+
+  questionField.innerHTML = questions[currentQuestion].text;
+
+  for (var i = 0; i < questions[currentQuestion].answers.length; i++) {
+    var answerField = document.getElementById("answerField" + i);
+
+    answerField.innerHTML = questions[currentQuestion].answers[i];
+
+
+  }
+}
 
 
 
@@ -60,12 +94,6 @@ var questions = [
     text: 'When was HTML created?',
     answers: ['1985', '2001', '1992', '1990'],
     correctAnswer: '1990'
-  }, 
-  {
-    number: 4,
-    text: 'this is a question?',
-    answers: ['answer one', 'answer two', 'answer three', 'answer 4'],
-    correctAnswer: 'answer one'
   },
   {
     number: 4,
@@ -74,33 +102,35 @@ var questions = [
     correctAnswer: 'answer one'
   },
   {
-    number: 4,
+    number: 5,
     text: 'this is a question?',
     answers: ['answer one', 'answer two', 'answer three', 'answer 4'],
     correctAnswer: 'answer one'
   },
   {
-    number: 4,
+    number: 6,
     text: 'this is a question?',
     answers: ['answer one', 'answer two', 'answer three', 'answer 4'],
     correctAnswer: 'answer one'
   },
   {
-    number: 4,
+    number: 7,
+    text: 'this is a question?',
+    answers: ['answer one', 'answer two', 'answer three', 'answer 4'],
+    correctAnswer: 'answer one'
+  },
+  {
+    number: 8,
     text: 'this is a question?',
     answers: ['answer one', 'answer two', 'answer three', 'answer 4'],
     correctAnswer: 'answer one'
   },
 
 ]
-function populateQuestion() {
-  // Grab one question from the questions array using currentQuestion index
-  //button with clickevent calls populatequestion and send populatequestion number
-  questionField.innerHTML=questions[0].text;
-  console.log(questions[0])
-  // This function will populate a question in the HTML dynamically
-}
+
+
+// function countdown()
 
 
 
-startGame();
+// startGame();
